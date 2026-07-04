7e25d2c436f893c66169218399ec7597805697da
jeffreydarlington <jeffrey2md@gmail.com>
2026-07-04 13:52:01 -0400

    Added return statements to some functions

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -1,4 +1,64 @@
 package ca.jrvs.apps.grep;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JavaGrepImp {
+
+    private String regex;
+    private String rootPath;
+    private String outFile;
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+    public String getRootPath() {
+        return rootPath;
+    }
+
+    public void setRootPath(String rootPath) {
+    }
+
+    public String getOutFile() {
+        return "";
+    }
+
+    public void setOutFile(String outFile) {
+
+    }
+
+    public void process() throws IOException {
+
+    }
+
+    public List<File> listFiles(String rootDir) {
+        List<File> filesList = new ArrayList<>();
+
+        return filesList;
+    }
+
+    public List<String> readLines(File inputFile) {
+        List<String> lines = new ArrayList<>();
+
+        return lines;
+    }
+
+    public boolean containsPattern(String line) {
+        return false;
+    }
+
+    public void writeToFile(List<String> lines) throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
